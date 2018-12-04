70e4857cbfc953bcab4bc169627a7ca8854e9a03
Hadi Kais <khadi@enseirb-matmeca.fr>
2018-12-04 16:28:46 +0100

    Achievement 2 : progression : The reader and the statistician works properly. the reader doesn't flag when he has finished a sentence

--- Achievement_2/main.c
+++ Achievement_2/main.c
@@ -4,24 +4,32 @@
 #include "Queue/queue.h"
 #include "Monkeyz/monkeyz.h"
 #include <time.h>
+#include <getopt.h>
 
-#define MAX_NUMBER_OF_ROUNDS 300
+#define MAX_NUMBER_OF_ROUNDS 5000
 
 
-int reading_arguments(int* seed_rng, FILE** read_file, int argc, char* argv[])
+int reading_arguments(int* seed_rng, FILE** read_file_1, FILE** read_file_2, int argc, char* argv[])
 {
-  for(int i = 1; i < argc; i++){
-    if(strcmp(argv[i],"-s") == 0){
-      if(i == argc - 1)
-        return 1; //Error
-      *seed_rng = atoi(argv[i+1]);
+    int opt;
+    while ((opt = getopt(argc, argv, "s:")) != -1) {
+        switch (opt) {
+            case 's':
+                *seed_rng = atoi(optarg);
+                break;
+        }
     }
-    if( (strcmp(argv[i-1],"-s") != 0) && (strcmp(argv[i],"-s") != 0) ){
-      *read_file = fopen(argv[i],"r");
+    if(argc == 4 || argc == 2){
+        *read_file_1 = fopen(argv[argc - 1],"r");
+        *read_file_2 = fopen(argv[argc - 1],"r");
+    }else{
+        *read_file_1 = fopen(argv[argc - 1],"r");
+        *read_file_2 = fopen(argv[argc - 2],"r");
     }
-  }
-  return 0;
+
+    return 0;
 }
+
 
 void print_args(int argc, char* argv[])
 {
@@ -52,72 +60,85 @@ void print_usage()
 int main(int argc, char* argv[])
 {
     //Initialization
-    FILE* read_file = NULL;
+    FILE* read_file_1 = NULL;
+    FILE* read_file_2 = NULL;
     int seed_rng = 0;
-    if(argc != 2 && argc != 4){
+
+    if(argc > 6 || argc == 1){
         print_usage();
-        return EXIT_SUCCESS;
+        exit(EXIT_SUCCESS);
     }
-    int error_code = reading_arguments(&seed_rng, &read_file, argc, argv);
+    int error_code = reading_arguments(&seed_rng, &read_file_1, &read_file_2, argc, argv);
     if(error_code == 1){
         printf("Erreur lors de la lecture des arguments\n");
-        return 3;
+        exit(EXIT_FAILURE);
     }
-    if(read_file == NULL){
+    if(read_file_1 == NULL){
         printf("Nom de fichier incorrect ou manquant\n");
-        return 1;
+        exit(EXIT_FAILURE);
     }
+    if(seed_rng == 0) //If no -s option has been declared
+        srand(time(NULL));
+    else
+        srand(seed_rng);
+
     struct queue reader_queue_a;
     struct queue reader_queue_b;
-    struct successors_queue stats_queue;
     struct queue writer_queue_a;
     struct queue writer_queue_b;
-    struct queue words_of_max_occurency;
-    struct queue words_of_min_occurency;
+    struct successors_queue stats_queue;
     init_queue(&reader_queue_a);
     init_queue(&reader_queue_b);
     init_queue(&writer_queue_a);
     init_queue(&writer_queue_b);
     init_successors_queue(&stats_queue);
-    init_queue(&words_of_max_occurency);
-    init_queue(&words_of_min_occurency);
+
     struct monkey monkeyz[NUMBER_OF_MONKEYS]; //Number of Monkeys is defined in monkeyz.h
-    init_monkeys(monkeyz, NUMBER_OF_MONKEYS,&writer_queue_a,&writer_queue_b,&reader_queue_a,&reader_queue_a);
+    init_monkeys(monkeyz, NUMBER_OF_MONKEYS,&writer_queue_a,&writer_queue_b,&reader_queue_a,&reader_queue_b,read_file_1,read_file_2);
+
     struct cell last_word_read;
     strcpy(last_word_read.word,"");
-    if(seed_rng == 0) //If no -s option has been declared
-        srand(time(NULL));
-    else
-        srand(seed_rng);
+
+
+    //Initializing the writers
+    strcpy(monkeyz[WRITER_1].memorized_word,".");
+    monkeyz[WRITER_1].sentence_length = 0;
+    strcpy(monkeyz[WRITER_2].memorized_word,".");
+    monkeyz[WRITER_2].sentence_length = 0;
     //End of Initialization
 
     //---
     // Main Algorithm
     //---
     int i = 0;
-    filter_active_monkeys(monkeyz, NUMBER_OF_MONKEYS, read_file, stats_queue);
+    filter_active_monkeys(monkeyz, NUMBER_OF_MONKEYS, stats_queue);
     while(!is_all_on_strike(monkeyz,NUMBER_OF_MONKEYS) && i < MAX_NUMBER_OF_ROUNDS){
         struct monkey* happy_selected_monkey = random_select(monkeyz, NUMBER_OF_MONKEYS, seed_rng);
-        if((happy_selected_monkey->work != WRITER_1 || happy_selected_monkey->work != WRITER_1)|| i > 100){
-          work(happy_selected_monkey, &stats_queue, read_file, &last_word_read, monkeyz);
+        if((happy_selected_monkey->work != WRITER_1 || happy_selected_monkey->work != WRITER_2)|| i > 100){
+          work(happy_selected_monkey, &stats_queue, &last_word_read, monkeyz);
         }
-      filter_active_monkeys(monkeyz, NUMBER_OF_MONKEYS, read_file, stats_queue);
+      filter_active_monkeys(monkeyz, NUMBER_OF_MONKEYS, stats_queue);
       i++;
     }
     //---
     // End of Main Algorithm
     //---
 
-    printf("\nRead words : %d \n",monkeyz[READER_1].read_words+monkeyz[READER_2].read_words);
+    // // print_queue_light(monkeyz[READER_1].my_queue);
+    // // print_queue_light(monkeyz[READER_2].my_queue);
+    // print_successors_queue(stats_queue);
+    // print_queue_light(monkeyz[WRITER_1].my_queue);
+    // print_queue_light(monkeyz[WRITER_2].my_queue);
+    // // printf("\nRead words : %d \n",monkeyz[READER_1].read_words+monkeyz[READER_2].read_words);
+
     //Purge
     purge_queue(&reader_queue_a);
     purge_queue(&reader_queue_b);
     purge_queue(&writer_queue_a);
     purge_queue(&writer_queue_b);
     purge_successors_queue(&stats_queue);
-    purge_queue(&words_of_max_occurency);
-    purge_queue(&words_of_min_occurency);
-    fclose(read_file);
+    fclose(read_file_1);
+    fclose(read_file_2);
     //End-Purge
 
     return EXIT_SUCCESS;
